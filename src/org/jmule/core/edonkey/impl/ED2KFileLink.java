df6cfa54869625ec6939848df29b8ed6622cb6be
binary256_ <binary256_>
2008-08-15 10:07:21 +0000

    Methods for extracting of ED2K file links from raw string added

--- src/org/jmule/core/edonkey/impl/ED2KFileLink.java
+++ src/org/jmule/core/edonkey/impl/ED2KFileLink.java
@@ -22,6 +22,8 @@
  */
 package org.jmule.core.edonkey.impl;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,8 +33,8 @@ import java.util.regex.Pattern;
  * ed2k://|file|NAME|SIZE|MD4-HASH|h=HASH|/
  * ed2k://|file|NAME|SIZE|MD4-HASH|/|sources,<IP:Port>,<IP:Port>,...|/
  * @author binary
- * @version $$Revision: 1.1 $$
- * Last changed by $$Author: javajox $$ on $$Date: 2008/07/31 16:43:34 $$
+ * @version $$Revision: 1.2 $$
+ * Last changed by $$Author: binary256_ $$ on $$Date: 2008/08/15 10:07:21 $$
  */
 public class ED2KFileLink {
 
@@ -42,7 +44,15 @@ public class ED2KFileLink {
 	
 	private FileHash fileHash;
 	
-
+	public ED2KFileLink(String fileName,long fileSize,FileHash fileHash) {
+		
+		this.fileName = fileName;
+		
+		this.fileSize = fileSize;
+		
+		this.fileHash = fileHash;
+		
+	}
 	
 	public ED2KFileLink(String fileLink){
 		
@@ -50,18 +60,64 @@ public class ED2KFileLink {
 		
 		Matcher m;
 		
-		s = Pattern.compile("ed2k:\\/\\/\\|(.*)\\|(.*)\\|([0-9]*)\\|(.*)\\|.*");
+		s = Pattern.compile("ed2k:\\/\\/\\|file\\|([^|]*)\\|([0-9]*)\\|([a-h0-9A-H]*)\\|\\/");
 		
 		m = s.matcher(fileLink);
 
 		if (m.matches()) {
 			
-			this.fileName=m.group(2);
+			this.fileName=m.group(1);
 			
-			this.fileSize=Long.valueOf(m.group(3)).longValue();
+			this.fileSize=Long.valueOf(m.group(2)).longValue();
 			
-			this.fileHash = new FileHash(m.group(4));
+			this.fileHash = new FileHash(m.group(3));
 	    }
+		
+	}
+	
+	public static List<ED2KFileLink> extractLinks(String rawData) {
+		
+		Pattern s;
+		
+		Matcher m;
+		
+		s = Pattern.compile("ed2k:\\/\\/\\|file\\|([^|]*)\\|([0-9]*)\\|([a-h0-9A-H]*)\\|\\/");
+		
+		m = s.matcher(rawData);
+		
+		List<ED2KFileLink> links = new LinkedList<ED2KFileLink>();
+		
+		while(m.find()) {
+			
+			String fileName = m.group(1);
+			
+			long fileSize = Long.valueOf(m.group(2)).longValue();
+			
+			FileHash fileHash = new FileHash(m.group(3));
+		
+			links.add(new ED2KFileLink(fileName,fileSize,fileHash));
+			
+		}
+		
+		return links;
+		
+	}
+	
+	public static boolean isValidLink(String link) {
+		Pattern s;
+		
+		Matcher m;
+		
+		s = Pattern.compile("ed2k:\\/\\/\\|file\\|([^|]*)\\|([0-9]*)\\|([a-h0-9A-H]*)\\|\\/");
+		
+		m = s.matcher(link);
+		
+		return m.matches();
+	}
+	
+	public String getAsString() {
+		
+		return "ed2k://file|"+fileName+"|"+fileSize+"|"+fileHash+"|/";
 		
 	}
 	
@@ -87,5 +143,13 @@ public class ED2KFileLink {
 		
 		return this.fileHash;
 		
+	}
+	
+	public static void main(String... args) {
+		String data = "ed2k://|file|zzz1.pdf|9496920|14AA4451445C1F45117F4E4676B863DA|/fsdsdsdffed2k://|file|zzz2.pdf|9496920|A4DA4451445C1F45157F4E4676B863DA|/ed2k://|file|zzz3.pdf|9496920|14AA4451445C1F45117F4E4676B863D4|/";
+		List<ED2KFileLink> list = ED2KFileLink.extractLinks(data);
+		for(ED2KFileLink link : list) {
+			System.out.println(link+" ");
+		}
 	}
 }
