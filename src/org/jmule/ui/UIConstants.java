8fe90b082adca9a5d9de84454dedbfd4c3e7653f
binary256_ <binary256_>
2008-08-19 19:45:37 +0000

    Added MymeType icon getter.

--- src/org/jmule/ui/UIConstants.java
+++ src/org/jmule/ui/UIConstants.java
@@ -22,14 +22,18 @@
  */
 package org.jmule.ui;
 
+import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * 
  * @author javajox
  * @author binary
- * @version $$Revision: 1.1 $$
- * Last changed by $$Author: javajox $$ on $$Date: 2008/07/31 16:44:56 $$
+ * @version $$Revision: 1.2 $$
+ * Last changed by $$Author: binary256_ $$ on $$Date: 2008/08/19 19:45:37 $$
  */
-public interface UIConstants {
+public class UIConstants {
 	
 	// Columns ID
 	public static final int SERVER_NAME_ID          =   100;
@@ -42,5 +46,68 @@ public interface UIConstants {
 	public static final int SERVER_SOFT_LIMIT_ID    =   800;
 	public static final int SERVER_HARD_LIMIT_ID    =   900;
 	public static final int SERVER_VERSION_ID   	=   1000;
+	
+	private static Map<String,String> icon_name_by_extension = new Hashtable<String,String>();
+
+	static {
+		icon_name_by_extension.put("rar","mimetypes/archive.png");
+		icon_name_by_extension.put("zip","mimetypes/archive.png");
+		icon_name_by_extension.put("tar","mimetypes/archive.png");
+		icon_name_by_extension.put("gz","mimetypes/archive.png");
+		icon_name_by_extension.put("7z","mimetypes/archive.png");
+		
+		icon_name_by_extension.put("mp3","mimetypes/audio.png");
+		icon_name_by_extension.put("wav","mimetypes/audio.png");
+		icon_name_by_extension.put("midi","mimetypes/audio.png");
+		icon_name_by_extension.put("midi","mimetypes/audio.png");
+		
+		icon_name_by_extension.put("iso","mimetypes/cd_image.png");
+		icon_name_by_extension.put("nrg","mimetypes/cd_image.png");
+		icon_name_by_extension.put("cue","mimetypes/cd_image.png");
+		
+		icon_name_by_extension.put("png","mimetypes/image.png");
+		icon_name_by_extension.put("jpg","mimetypes/image.png");
+		icon_name_by_extension.put("jpeg","mimetypes/image.png");
+		icon_name_by_extension.put("gif","mimetypes/image.png");
+		icon_name_by_extension.put("bmp","mimetypes/image.png");
+		icon_name_by_extension.put("ico","mimetypes/image.png");
+		
+		icon_name_by_extension.put("pdf","mimetypes/pdf.png");
+		
+		icon_name_by_extension.put("doc","mimetypes/doc.png");
+		icon_name_by_extension.put("wri","mimetypes/doc.png");
+		icon_name_by_extension.put("odt","mimetypes/doc.png");
+		icon_name_by_extension.put("sxw","mimetypes/doc.png");
+		icon_name_by_extension.put("vor","mimetypes/doc.png");
+		
+		icon_name_by_extension.put("xls","mimetypes/calc.png");
+		
+		icon_name_by_extension.put("exe", "mimetypes/windows_exe.png");
+		icon_name_by_extension.put("com", "mimetypes/windows_exe.png");
+		icon_name_by_extension.put("bat", "mimetypes/windows_exe.png");
+		icon_name_by_extension.put("cmd", "mimetypes/windows_exe.png");
+		
+		icon_name_by_extension.put("so", "mimetypes/executable.png");
+		icon_name_by_extension.put("bin", "mimetypes/executable.png");
+		icon_name_by_extension.put("sh", "mimetypes/executable.png");
+		
+		icon_name_by_extension.put("mpg", "mimetypes/video.png");
+		icon_name_by_extension.put("mpeg", "mimetypes/video.png");
+		icon_name_by_extension.put("avi", "mimetypes/video.png");
+		icon_name_by_extension.put("wmv", "mimetypes/video.png");
+		icon_name_by_extension.put("bik", "mimetypes/video.png");
+		icon_name_by_extension.put("mov", "mimetypes/video.png");
+	}
+	
+	  public static final String INFINITY_STRING	= "\u221E"; // "oo";
+	  public static final int    INFINITY_AS_INT = 31536000; // seconds (365days)
+	
+	public static InputStream getIconByExtension(String extension) {
+		extension = extension.toLowerCase();
+		String image_path = icon_name_by_extension.get(extension);
+		if (image_path == null)
+			image_path = "mimetypes/default.png";
+		return UIImageRepository.getImageAsStream(image_path);
+	}
 	
 }
