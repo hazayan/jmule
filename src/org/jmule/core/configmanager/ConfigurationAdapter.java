fdbc646bb4e8c3d0ee113d24c45bc91bd3b78356
binary256_ <binary256_>
2008-08-27 05:44:38 +0000

    Initial import

---
+++ src/org/jmule/core/configmanager/ConfigurationAdapter.java
@@ -0,0 +1,76 @@
+/*
+ *  JMule - Java file sharing client
+ *  Copyright (C) 2007-2008 JMule team ( jmule@jmule.org / http://jmule.org )
+ *
+ *  Any parts of this program derived from other projects, or contributed
+ *  by third-party developers are copyrighted by their respective authors.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+package org.jmule.core.configmanager;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created on Aug 22, 2008
+ * @author binary256
+ * @version $Revision: 1.1 $
+ * Last changed by $Author: binary256_ $ on $Date: 2008/08/27 05:44:38 $
+ */
+public abstract class ConfigurationAdapter implements ConfigurationListener {
+
+	public void TCPPortChanged(int tcp) {
+		
+	}
+
+	public void UDPPortChanged(int udp) {
+		
+	}
+
+	public void downloadBandwidthChanged(long downloadBandwidth) {
+		
+	}
+
+	public void downloadLimitChanged(long downloadLimit) {
+		
+	}
+
+	public void isUDPEnabledChanged(boolean enabled) {
+		
+	}
+
+	public void nickNameChanged(String nickName) {
+		
+	}
+
+	public void sharedDirectoriesChanged(List<File> sharedDirs) {
+		
+	}
+
+	public void uploadBandwidthChanged(long uploadBandwidth) {
+		
+	}
+
+	public void uploadLimitChanged(long uploadLimit) {
+		
+	}
+
+	public void parameterChanged(String parameter, Object newValue) {
+
+	}
+
+}
