f768794a4cda8435d4eb66790c5b56073079a2c4
binary256_ <binary256_>
2008-09-07 16:49:40 +0000

    POPUP menu added

--- src/org/jmule/ui/swt/maintabs/serverlist/ServerMessages.java
+++ src/org/jmule/ui/swt/maintabs/serverlist/ServerMessages.java
@@ -23,44 +23,102 @@
 package org.jmule.ui.swt.maintabs.serverlist;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MenuDetectEvent;
+import org.eclipse.swt.events.MenuDetectListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Text;
 import org.jmule.ui.JMuleUI;
 import org.jmule.ui.JMuleUIManager;
+import org.jmule.ui.localizer._;
+import org.jmule.ui.swt.SWTImageRepository;
+import org.jmule.ui.swt.SWTThread;
 import org.jmule.ui.swt.Utils;
 import org.jmule.ui.swt.skin.SWTSkin;
 
 /**
  * 
  * @author binary256
- * @version $$Revision: 1.1 $$
- * Last changed by $$Author: javajox $$ on $$Date: 2008/07/31 16:44:11 $$
+ * @version $$Revision: 1.2 $$
+ * Last changed by $$Author: binary256_ $$ on $$Date: 2008/09/07 16:49:40 $$
  */
 public class ServerMessages extends Text {
 
 	public ServerMessages(Composite composite) {
 		super(composite,SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		
 		JMuleUI ui_instance = null;
 		try {
-			
 		    ui_instance = JMuleUIManager.getJMuleUI();
-		
 		}catch(Throwable t) {
 		}
-		
 		SWTSkin skin = (SWTSkin)ui_instance.getSkin();
 		
 		setFont(skin.getDefaultFont());
 		setEditable(false);
-		setBackground(Utils.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		setBackground(SWTThread.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		SWTServerListWrapper.getInstance().setServerMessages(this);
 		
+		Menu popup_menu = new Menu(this);
+		
+		final MenuItem copy_selected = new MenuItem(popup_menu,SWT.PUSH);
+		copy_selected.setText(_._("mainwindow.serverlisttab.servermesasges.popupmenu.copy"));
+		copy_selected.setImage(SWTImageRepository.getImage("copy.png"));
+		copy_selected.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				String text = getSelectionText();
+				if (text.length()!=0)
+					Utils.setClipBoardText(text);
+			}
+		});
+		
+		final MenuItem select_all = new MenuItem(popup_menu,SWT.PUSH);
+		select_all.setText(_._("mainwindow.serverlisttab.servermesasges.popupmenu.select_all"));
+		select_all.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				selectAll();
+			}
+		});
+		
+		new MenuItem(popup_menu,SWT.SEPARATOR);
+		
+		final MenuItem clear = new MenuItem(popup_menu,SWT.PUSH);
+		clear.setText(_._("mainwindow.serverlisttab.servermesasges.popupmenu.clear"));
+		clear.setImage(SWTImageRepository.getImage("remove_all.png"));
+		clear.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				setText("");
+			}
+		});
+		
+		setMenu(popup_menu);
+		
+		addMenuDetectListener(new MenuDetectListener() {
+
+			public void menuDetected(MenuDetectEvent arg0) {
+				if (getText().length()==0) {
+					clear.setEnabled(false);
+					select_all.setEnabled(false);
+				}
+				else {
+					clear.setEnabled(true);
+					select_all.setEnabled(true);
+				}
+				
+				if (getSelectionCount()==0)
+					copy_selected.setEnabled(false);
+				else
+					copy_selected.setEnabled(true);
+			}
+			
+		});
 	}
 	
 	public void addText(String message) {
-		String str = getText()+"\n"+message;
-		setText(str);
+		append(message+"\n");
+		setSelection(getText().length());
 	}
 	
 	protected void checkSubclass() {
