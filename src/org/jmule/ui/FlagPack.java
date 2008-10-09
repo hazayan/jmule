4c14d05418924bf754e97ecf3f143bdfadd36e73
binary256_ <binary256_>
2008-10-09 16:48:55 +0000

    Added :  * getFolderBySize()  * getFlagAsInputStreamByIP()  * getFlagAsInputStream()

--- src/org/jmule/ui/FlagPack.java
+++ src/org/jmule/ui/FlagPack.java
@@ -22,14 +22,11 @@
  */
 package org.jmule.ui;
 
-import java.awt.GridLayout;
-import java.net.InetAddress;
+import java.io.InputStream;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 import org.jmule.countrylocator.CountryLocator;
 
@@ -37,46 +34,76 @@ import org.jmule.countrylocator.CountryLocator;
  *
  * Created on Oct 6, 2008
  * @author javajox
- * @version $Revision: 1.1 $
- * Last changed by $Author: javajox $ on $Date: 2008/10/09 15:10:24 $
+ * @version $Revision: 1.2 $
+ * Last changed by $Author: binary256_ $ on $Date: 2008/10/09 16:48:55 $
  */
 public class FlagPack {
-
-	private static String UNKNOWN_COUNTRY = "unknown";
-	private static String FLAG_FILE_EXT = ".png";
+	private static String FLAG_FILE_EXT 	   = ".png";
+	private static String UNKNOWN_COUNTRY_FLAG = "unknown_cc.png"; 
+	
 	public static String FLAGS_FOLDER18x25 = "org/jmule/ui/resources/" + "flags18x12" + "/";
 	public static String FLAGS_FOLDER25x15 = "org/jmule/ui/resources/" + "flags25x15" + "/";
-	
+	 
 	public enum FlagSize {
 		S18x25, S25x15
 	}
 	
 	public static URL getFlagAsURL(String countryCode, FlagSize flag_size) {
 		 if(countryCode.compareTo(CountryLocator.COUNTRY_CODE_NOT_AVAILABLE) == 0)	 
-		     return null;
+			 return FlagPack.class.getClassLoader().getResource(
+						getFolderBySize(flag_size) +UNKNOWN_COUNTRY_FLAG);
 		
 	  URL image_url = null; 
-		 
+	  
 	  try {	
 		  
 		switch(flag_size) {
 		   case  S18x25  : 	image_url = FlagPack.class.getClassLoader().getResource(
-				                            FLAGS_FOLDER18x25 + 
+				                            getFolderBySize(flag_size) + 
                                             countryCode.toLowerCase() + 
                                             FLAG_FILE_EXT);
 			               return  image_url;
 			               
 		   case S25x15   : image_url = FlagPack.class.getClassLoader().getResource(
-				                           FLAGS_FOLDER25x15 + 
+				   						   getFolderBySize(flag_size) + 
                                            countryCode.toLowerCase() + 
                                            FLAG_FILE_EXT);
 			               return  image_url;
 		}
 	  } catch(Throwable t) {
 		  
-		  return null;
+		  return FlagPack.class.getClassLoader().getResource(
+					getFolderBySize(flag_size) +UNKNOWN_COUNTRY_FLAG);
 	  }
-		return null;
+	  return FlagPack.class.getClassLoader().getResource(
+				getFolderBySize(flag_size) +UNKNOWN_COUNTRY_FLAG);
+	}
+	
+	public static InputStream getFlagAsInputStream(String countryCode, FlagSize flag_size) {
+		 if(countryCode.compareTo(CountryLocator.COUNTRY_CODE_NOT_AVAILABLE) == 0)	 
+			 return FlagPack.class.getClassLoader().getResourceAsStream(
+						getFolderBySize(flag_size) +UNKNOWN_COUNTRY_FLAG);
+		
+	  try {	
+		  
+		switch(flag_size) {
+		   case  S18x25  : 	return FlagPack.class.getClassLoader().getResourceAsStream(
+				   							getFolderBySize(flag_size) + 
+                                           countryCode.toLowerCase() + 
+                                           FLAG_FILE_EXT);
+			               
+		   case S25x15   : return FlagPack.class.getClassLoader().getResourceAsStream(
+				   							getFolderBySize(flag_size) + 
+                                          countryCode.toLowerCase() + 
+                                          FLAG_FILE_EXT);
+		}
+	  } catch(Throwable t) {
+		  
+		  return FlagPack.class.getClassLoader().getResourceAsStream(
+						getFolderBySize(flag_size) +UNKNOWN_COUNTRY_FLAG);
+	  }
+	  return FlagPack.class.getClassLoader().getResourceAsStream(
+				getFolderBySize(flag_size) +UNKNOWN_COUNTRY_FLAG);
 	}
 	
 	public static Icon getFlagAsIcon(String countryCode, FlagSize flag_size) {
@@ -101,6 +128,16 @@ public class FlagPack {
 	
 	public static URL getFlagAsURLByIP(Object inetAddress, FlagSize flagSize) {
         return 	getFlagAsURL(CountryLocator.getInstance().getCountryCode(inetAddress), flagSize);	
+	}
+	
+	public static InputStream getFlagAsInputStreamByIP(Object inetAddress,FlagSize flagSize) {
+		return getFlagAsInputStream(CountryLocator.getInstance().getCountryCode(inetAddress), flagSize);
+	}
+	
+	
+	private static String getFolderBySize(FlagSize flag_size) {
+		if (flag_size == FlagSize.S18x25) return FLAGS_FOLDER18x25;
+		return FLAGS_FOLDER25x15;
 	}
 	
 }
