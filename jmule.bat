80ccdee2667c5978a609a50520e7331720cb22b7
binary256_ <binary256_>
2008-10-16 18:58:47 +0000

    Classpath updated

--- jmule.bat
+++ jmule.bat
@@ -1,1 +1,1 @@
-java -jar jmule.jar
+java -cp .\lib\swt-windows\swt.jar;.\lib\swingx-0.9.3.jar;jmule.jar;. org.jmule.main.Main -Xms 128M
--- jmule.sh
+++ jmule.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp lib/swt.jar:jmule.jar:. org.jmule.main.Main
+java -cp lib/swt-linux/swt.jar:lib/swingx-0.9.3.jar:jmule.jar:. org.jmule.main.Main -Xms 128M
