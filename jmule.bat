f90bc273699bdffe07f27dde2853b30b3651b7bb
binary256_ <binary256_>
2008-10-19 18:56:14 +0000

    Updated maximum heap memory

--- jmule.bat
+++ jmule.bat
@@ -1,1 +1,1 @@
-java -cp .\lib\swt-windows\swt.jar;.\lib\swingx-0.9.3.jar;jmule.jar;. org.jmule.main.Main -Xms 128M
+java -Xmx128m  -cp .\lib\swt-windows\swt.jar;.\lib\swingx-0.9.3.jar;jmule.jar;. org.jmule.main.Main
--- jmule.sh
+++ jmule.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp lib/swt-linux/swt.jar:lib/swingx-0.9.3.jar:jmule.jar:. org.jmule.main.Main -Xms 128M
+java -Xmx128m -cp lib/swt-linux/swt.jar:lib/swingx-0.9.3.jar:jmule.jar:. org.jmule.main.Main
