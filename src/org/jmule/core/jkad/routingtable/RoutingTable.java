a567e7a813a18841e6a412f7f2f7d985e0c63c85
binary255 <binary255>
2009-07-26 06:14:57 +0000

    Changed RoutingTable maintenance algorithm.

--- src/org/jmule/core/jkad/routingtable/RoutingTable.java
+++ src/org/jmule/core/jkad/routingtable/RoutingTable.java
@@ -22,17 +22,14 @@
  */
 package org.jmule.core.jkad.routingtable;
 
-import static org.jmule.core.jkad.JKadConstants.CONTACTS_SEND_HELLO;
 import static org.jmule.core.jkad.JKadConstants.MAX_CONTACTS;
 import static org.jmule.core.jkad.JKadConstants.NODES_DAT;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_CHECK_INTERVAL;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_CONTACTS_CHECK_INTERVAL;
-import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_CONTACT_ACCEP_TIME;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_CONTACT_IGNORE_TIME;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_CONTACT_TIMEOUT;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_DIFICIT_CONTACTS;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_DIFICIT_CONTACTS_STOP;
-import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_HELLO_SEND_INTERVAL;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_MAINTENANCE_CONTACTS;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_MAX_MAINTENANCE_CONTACTS;
 import static org.jmule.core.jkad.JKadConstants.ROUTING_TABLE_SAVE_INTERVAL;
@@ -47,12 +44,18 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.jmule.core.edonkey.packet.tag.TagList;
 import org.jmule.core.jkad.ContactAddress;
 import org.jmule.core.jkad.IPAddress;
 import org.jmule.core.jkad.Int128;
+import org.jmule.core.jkad.JKad;
+import org.jmule.core.jkad.JKadConstants;
+import org.jmule.core.jkad.PacketListener;
+import org.jmule.core.jkad.JKadConstants.ContactType;
 import org.jmule.core.jkad.JKadConstants.RequestType;
 import org.jmule.core.jkad.logger.Logger;
 import org.jmule.core.jkad.lookup.Lookup;
@@ -68,8 +71,8 @@ import org.jmule.core.net.JMUDPConnection;
 /**
  * Created on Dec 28, 2008
  * @author binary256
- * @version $Revision: 1.4 $
- * Last changed by $Author: binary255 $ on $Date: 2009/07/15 18:05:34 $
+ * @version $Revision: 1.5 $
+ * Last changed by $Author: binary255 $ on $Date: 2009/07/26 06:14:57 $
  */
 public class RoutingTable {
 
@@ -79,12 +82,14 @@ public class RoutingTable {
 	private List<KadContact> tree_nodes = new CopyOnWriteArrayList<KadContact>();
 	private Task maintenanceTask = null;
 	private Task routingTableSave = null;
-	private Task helloSender = null;
 	private Task contact_checker = null;
-	//private Task fakeLookup = null;
+	
 	private int newContacts = 0;
 	
 	private List<RoutingTableListener> listener_list = new CopyOnWriteArrayList<RoutingTableListener>();
+	
+	private Map<ContactAddress,MaintenanceContact> maintenanceContacts = new ConcurrentHashMap<ContactAddress,MaintenanceContact>();
+	private PacketListener helloListener;
 	
 	public static RoutingTable getSingleton() {
 		if (singleton == null)
@@ -102,7 +107,6 @@ public class RoutingTable {
 		newContacts = 0;
 		
 		maintenanceTask = new Task() {
-			private List<KadContact> maintenanceContacts = new CopyOnWriteArrayList<KadContact>();
 			private LookupTask lookup_new_contacts = null;
 			public void run() {
 				if (getTotalContacts() < ROUTING_TABLE_DIFICIT_CONTACTS) {
@@ -130,22 +134,40 @@ public class RoutingTable {
 						Lookup.getSingleton().addLookupTask(lookup_new_contacts);
 					}
 				}
-				
-				for(KadContact contact : maintenanceContacts ) {
+								
+				for(MaintenanceContact maintenance_contact : maintenanceContacts.values()) {
+					KadContact contact = maintenance_contact.kadContact;
+					
 					long contact_time = System.currentTimeMillis() - contact.getLastResponse();
 					if (contact_time >= ROUTING_TABLE_CONTACT_TIMEOUT) {
-						System.out.println("Remove contact \n" + contact);
-						maintenanceContacts.remove(contact);
-						removeNode(contact);
+						int rcount = maintenance_contact.requestCount;
+						if (rcount<=contact.getContactType().toByte()+1) {
+							KadPacket hello_packet = PacketFactory.getHello2ReqPacket(TagList.EMPTY_TAG_LIST);
+							JMUDPConnection.getInstance().sendPacket(hello_packet, maintenance_contact.kadContact.getIPAddress(), maintenance_contact.kadContact.getUDPPort());
+							maintenance_contact.requestCount++;
+							continue;
+						}
+						
+						int beta = maintenance_contact.requestCount - maintenance_contact.responseCount;
+						boolean downgrate = false;
+						if (beta>contact.getContactType().toByte()+1)
+							downgrate = true;
+						
+						if (downgrate) {
+							if (contact.getContactType() == ContactType.ScheduledForRemoval) {
+								maintenanceContacts.remove(contact.getContactAddress());
+								removeNode(contact);
+								continue;
+							}
+							contact.downgrateType();
+							contactUpdated(contact);
+							if (contact.getContactType()!=ContactType.ScheduledForRemoval)
+								maintenanceContacts.remove(contact.getContactAddress());
+							continue;
+						}
+						maintenanceContacts.remove(contact.getContactAddress());
 						continue;
 					}
-					
-					if (contact_time <= ROUTING_TABLE_CONTACT_ACCEP_TIME) {
-						maintenanceContacts.remove(contact);
-						System.out.println("Update contact : \n" + contact);
-					}
-					
-					
 					
 				}
 				if (maintenanceContacts.size()>=ROUTING_TABLE_MAX_MAINTENANCE_CONTACTS) return ;
@@ -156,12 +178,14 @@ public class RoutingTable {
 					do {
 						int id = Utils.getRandom(candidatList.size());
 						addContact = candidatList.get(id);
-					}while(maintenanceContacts.contains(addContact));
-					
-					maintenanceContacts.add(addContact);
+						
+					}while(maintenanceContacts.containsKey(addContact.getContactAddress()));
+					MaintenanceContact c = new MaintenanceContact(addContact);
+					maintenanceContacts.put(addContact.getContactAddress(), c);
 					
 					KadPacket hello_packet = PacketFactory.getHello2ReqPacket(TagList.EMPTY_TAG_LIST);
 					JMUDPConnection.getInstance().sendPacket(hello_packet, addContact.getIPAddress(), addContact.getUDPPort());
+					c.requestCount++;
 				}
 			}
 			
@@ -176,18 +200,6 @@ public class RoutingTable {
 		};
 		
 		Timer.getSingleton().addTask(ROUTING_TABLE_SAVE_INTERVAL, routingTableSave, true);
-		
-		helloSender = new Task() {
-			public void run() {
-				List<KadContact> list = getRandomContacts(CONTACTS_SEND_HELLO);
-				
-				for(KadContact contact : list) {
-					KadPacket hello_packet = PacketFactory.getHello2ReqPacket(TagList.EMPTY_TAG_LIST);
-					JMUDPConnection.getInstance().sendPacket(hello_packet, contact.getIPAddress(), contact.getUDPPort());
-				}
-			}
-		};
-		Timer.getSingleton().addTask(ROUTING_TABLE_HELLO_SEND_INTERVAL, helloSender, true);
 		
 		contact_checker = new Task() {
 			public void run() {
@@ -228,33 +240,26 @@ public class RoutingTable {
 		};
 		Timer.getSingleton().addTask(ROUTING_TABLE_CONTACTS_CHECK_INTERVAL, contact_checker, true);
 		
-		/*fakeLookup = new Task() {
-			public void run() {
-				Int128 clientID = new Int128(Utils.getRandomInt128());
-				Int128 toleranceZone = new Int128();
-				toleranceZone.setBit(127, true);
-				if (find_node_task!=null)
-					find_node_task.stopLookup();
-				find_node_task = new LookupTask(RequestType.FIND_NODE, clientID, toleranceZone) {
-					public void lookupTimeout() {
-					}
-					public void processToleranceContacts(ContactAddress sender,
-							List<KadContact> results) {
-						for(KadContact contact : results)
-							addContact(contact);
-					}
+		helloListener = new PacketListener(JKadConstants.KADEMLIA2_HELLO_RES) {
+			public void packetReceived(KadPacket packet) {
+				ContactAddress address = new ContactAddress(packet.getAddress());
+				if (maintenanceContacts.containsKey(address)) {
+					MaintenanceContact contact = maintenanceContacts.get(address);
+					contact.responseCount++;
+				}
 					
-				};
-				Lookup.getSingleton().addLookupTask(find_node_task);
-			}
+			}		
 		};
-		Timer.getSingleton().addTask(ROUTING_TABLE_FAKE_LOOKUP_INTERVAL, fakeLookup, true);*/
+		
+		JKad.getInstance().addPacketListener(helloListener);
 	}
 	
 	public void stop() {
+		JKad.getInstance().removePacketListener(helloListener);
+		
 		Timer.getSingleton().removeTask(maintenanceTask);
 		Timer.getSingleton().removeTask(routingTableSave);
-		Timer.getSingleton().removeTask(helloSender);
+			
 		Timer.getSingleton().removeTask(contact_checker);
 		
 		tree_nodes.clear();
@@ -466,7 +471,6 @@ public class RoutingTable {
 	
 	
 	private void contactUpdated(KadContact contact) {
-		System.out.println("Updated contact information : \n"+contact);
 		for(RoutingTableListener listener : listener_list)
 			listener.contactUpdated(contact);
 	}
@@ -484,6 +488,20 @@ public class RoutingTable {
 	private void allContactsRemoved() {
 		for(RoutingTableListener listener : listener_list)
 			listener.allContactsRemoved();
+	}
+	
+	class MaintenanceContact {
+		public KadContact kadContact = null;
+		public int requestCount = 0;
+		public int responseCount = 0;
+		
+		public MaintenanceContact(KadContact kadContact) {
+			this.kadContact = kadContact;
+		}	
+		
+		public String toString() {
+			return kadContact.getContactAddress()+" " + requestCount + " " + responseCount;
+		}
 	}
 	
 }
