2f0a89bc479cbdc227ab155b4a9a48a10d5da42f
binary255 <binary255>
2009-09-17 18:34:31 +0000

    *** empty log message ***

--- src/org/jmule/main/Launcher.java
+++ src/org/jmule/main/Launcher.java
@@ -28,8 +28,8 @@ import org.jmule.core.JMuleCoreFactory;
 import org.jmule.core.JMuleCoreLifecycleAdapter;
 import org.jmule.core.configmanager.ConfigurationManager;
 import org.jmule.core.downloadmanager.DownloadManager;
-import org.jmule.core.edonkey.ServerManager;
 import org.jmule.core.peermanager.PeerManager;
+import org.jmule.core.servermanager.ServerManager;
 import org.jmule.core.sharingmanager.SharingManager;
 import org.jmule.core.uploadmanager.UploadManager;
 import org.jmule.ui.CommonUIPreferences;
@@ -43,106 +43,102 @@ import org.jmule.ui.swt.SWTThread;
 /**
  * 
  * @author javajox
- * @version $$Revision: 1.2 $$
- * Last changed by $$Author: javajox $$ on $$Date: 2008/10/16 17:36:32 $$
+ * @version $$Revision: 1.3 $$
+ * Last changed by $$Author: binary255 $$ on $$Date: 2009/09/17 18:34:31 $$
  */
 public class Launcher {
 
 	Splash splash = null;
-	
+
 	public Launcher() {
-		
+
 		try {
-			
+
 			CommonUIPreferences _pref = CommonUIPreferences.getSingleton();
-			
-			
-			if(_pref.getUIType().equals(JMuleUIManager.SWING_UI)) {
-				splash = new JSplash();	
+
+			if (_pref.getUIType().equals(JMuleUIManager.SWING_UI)) {
+				splash = new JSplash();
 			}
-			
-			if(_pref.getUIType().equals(JMuleUIManager.SWT_UI)) {
+
+			if (_pref.getUIType().equals(JMuleUIManager.SWT_UI)) {
 				// first of all we must start the swt thread
 				SWTThread.getInstance().initialize();
 				SWTThread.getInstance().start();
 				splash = new SWTSplash();
 			}
-			
+
 			splash.splashOn();
-			
+
 			splash.increaseProgress(5, "Initialize the system");
-			
+
 			SetupWizard setup_wizard;
-			
+
 			JMuleCoreFactory.create();
-			
+
 			JMuleCore _core = JMuleCoreFactory.getSingleton();
-			
+
 			JMuleCoreLifecycleAdapter core_lifecycle_listener = new JMuleCoreLifecycleAdapter() {
-				
+
 				public void componentStarted(JMuleCoreComponent component) {
-					
-					       if(component instanceof ConfigurationManager)
-					         splash.increaseProgress(10, "Config manager started");
-					  else if(component instanceof SharingManager)
-					         splash.increaseProgress(10, "Sharing manager started");
-					  else if(component instanceof UploadManager) 
-						     splash.increaseProgress(10, "Upload manager started");
-					  else if(component instanceof PeerManager) 
-						     splash.increaseProgress(10, "Peer manager started");
-					  else if(component instanceof DownloadManager)
-						     splash.increaseProgress(10, "Download manager started");
-					  else if(component instanceof ServerManager)
-						     splash.increaseProgress(10, "Server manager started");
-					
+
+					if (component instanceof ConfigurationManager)
+						splash.increaseProgress(10, "Config manager started");
+					else if (component instanceof SharingManager)
+						splash.increaseProgress(10, "Sharing manager started");
+					else if (component instanceof UploadManager)
+						splash.increaseProgress(10, "Upload manager started");
+					else if (component instanceof PeerManager)
+						splash.increaseProgress(10, "Peer manager started");
+					else if (component instanceof DownloadManager)
+						splash.increaseProgress(10, "Download manager started");
+					else if (component instanceof ServerManager)
+						splash.increaseProgress(10, "Server manager started");
+
 				}
-				
+
 			};
-			
-			_core.addLifecycleListener( core_lifecycle_listener );
-			
+
+			_core.addLifecycleListener(core_lifecycle_listener);
+
 			_core.start();
-			
+
 			splash.increaseProgress(10, "JMule core started");
-			
-			_core.removeLifecycleListener( core_lifecycle_listener );
-			
+
+			_core.removeLifecycleListener(core_lifecycle_listener);
+
 			boolean is_core_first_run = _core.isFirstRun();
-			
-			if( ! is_core_first_run ) {
-				
+
+			if (!is_core_first_run) {
+
 				splash.increaseProgress(5, "Starting JMule UI manager");
-				
+
 				splash.splashOff();
-				
+
 				JMuleUIManager.create();
-				
+
 			}
-			
-			if( is_core_first_run ) {
-				
+
+			if (is_core_first_run) {
+
 				splash.increaseProgress(5, "Running setup wizard");
-				
-				//setup_wizard = new SetupWizard(splash);
-				
+
+				// setup_wizard = new SetupWizard(splash);
+
 				setup_wizard = new SetupWizard();
-				
+
 				setup_wizard.setAlwaysOnTop(true);
-				
+
 				setup_wizard.setVisible(true);
-				
+
 				splash.splashOff();
 			}
-			
-			
-			
-		}catch(Throwable t) {
-			
-			
+
+		} catch (Throwable t) {
+
 			t.printStackTrace();
-			
+
 		}
-		
+
 	}
-	
+
 }
