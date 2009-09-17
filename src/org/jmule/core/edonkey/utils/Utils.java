d395989824a744ebd1a9e5fc0ec0b3033b09e4d5
binary255 <binary255>
2009-09-17 17:54:33 +0000

    Added methods : scanTCPServerFeatures() scanUDPFeatures()

--- src/org/jmule/core/edonkey/utils/Utils.java
+++ src/org/jmule/core/edonkey/utils/Utils.java
@@ -22,7 +22,7 @@
  */
 package org.jmule.core.edonkey.utils;
 
-import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.AICHVer;
+import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.*;
 import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.AcceptCommentVer;
 import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.DataCompVer;
 import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.ExtendedRequestsVer;
@@ -35,20 +35,23 @@ import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.SupportSecIdent;
 import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.UDPVer;
 import static org.jmule.core.edonkey.E2DKConstants.PeerFeatures.UnicodeSupport;
 
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.Set;
 
-import org.jmule.core.edonkey.E2DKConstants.PeerFeatures;
+import static org.jmule.core.edonkey.E2DKConstants.*;
+import org.jmule.core.edonkey.E2DKConstants.ServerFeatures;
 
 /**
  * Created on Dec 24, 2008
  * @author binary256
- * @version $Revision: 1.1 $
- * Last changed by $Author: binary255 $ on $Date: 2009/05/09 16:41:26 $
+ * @version $Revision: 1.2 $
+ * Last changed by $Author: binary255 $ on $Date: 2009/09/17 17:54:33 $
  */
 public class Utils {
 
-	public static int MiscOptions1ToInt( Map<PeerFeatures, Integer> clientFeatures) {
+	public static int peerFeaturesToInt( Map<PeerFeatures, Integer> clientFeatures) {
 		int misc_optins1 = 0;
 		
 		misc_optins1 |= (byte)(clientFeatures.get(AICHVer) << 29);
@@ -67,7 +70,7 @@ public class Utils {
 		return misc_optins1;
 	}
 	
-	public static Map<PeerFeatures,Integer> IntToMiscOptions1(int rawData) {
+	public static Map<PeerFeatures,Integer> scanTCPPeerFeatures(int rawData) {
 		Map<PeerFeatures,Integer> result = new Hashtable<PeerFeatures,Integer>();
 		
 		result.put(AICHVer, (rawData >> 29) & 0x07);
@@ -82,6 +85,63 @@ public class Utils {
 		result.put(NoViewSharedFiles, (rawData >>  2) & 0x01);
 		result.put(MultiPacket, (rawData >>  1) & 0x01);
 		result.put(SupportPreview, (rawData >>  0) & 0x01);
+		
+		return result;
+	}
+	
+	public static Set<ServerFeatures> scanTCPServerFeatures(int serverFeatures) {
+		Set<ServerFeatures> result = new HashSet<ServerFeatures>();
+		
+		if ((serverFeatures & SRV_TCPFLG_COMPRESSION) != 0) 
+			result.add(ServerFeatures.Compression);
+
+		if ((serverFeatures & SRV_TCPFLG_NEWTAGS) != 0) 
+			result.add(ServerFeatures.NewTags);
+		
+		if ((serverFeatures & SRV_TCPFLG_UNICODE) != 0) 
+			result.add(ServerFeatures.Unicode);
+		
+		if ((serverFeatures & SRV_TCPFLG_RELATEDSEARCH) != 0) 
+			result.add(ServerFeatures.RelatedSearch);
+		
+		if ((serverFeatures & SRV_TCPFLG_TYPETAGINTEGER) != 0) 
+			result.add(ServerFeatures.TypeTagInteger);
+		
+		if ((serverFeatures & SRV_TCPFLG_LARGEFILES) != 0) 
+			result.add(ServerFeatures.LargeFiles);
+		
+		if ((serverFeatures & SRV_TCPFLG_TCPOBFUSCATION) != 0) 
+			result.add(ServerFeatures.TCPObfusication);
+		
+		return result;
+	}
+	
+	public static Set<ServerFeatures> scanUDPFeatures(int serverUDPFeatures) {
+		Set<ServerFeatures> result = new HashSet<ServerFeatures>();
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_EXT_GETSOURCES) != 0) 
+			result.add(ServerFeatures.GetSources);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_EXT_GETFILES) != 0) 
+			result.add(ServerFeatures.GetFiles);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_NEWTAGS) != 0) 
+			result.add(ServerFeatures.NewTags);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_UNICODE) != 0) 
+			result.add(ServerFeatures.Unicode);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_EXT_GETSOURCES2) != 0) 
+			result.add(ServerFeatures.GetSources2);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_LARGEFILES) != 0) 
+			result.add(ServerFeatures.LargeFiles);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_UDPOBFUSCATION) != 0) 
+			result.add(ServerFeatures.UDPObfusication);
+		
+		if ((serverUDPFeatures & SRV_UDPFLG_TCPOBFUSCATION) != 0) 
+			result.add(ServerFeatures.TCPObfusication);
 		
 		return result;
 	}
