b281993295f6e55219a55a1f578bc7ba42bcd8ac
binary255 <binary255>
2010-07-06 08:44:16 +0000

    Obsolete synchronization replaced with lock object synchronization

--- src/org/jmule/core/edonkey/metfile/PartMet.java
+++ src/org/jmule/core/edonkey/metfile/PartMet.java
@@ -97,8 +97,8 @@ import org.jmule.core.utils.Misc;
  *
  * Created on Nov 7, 2007
  * @author binary256
- * @version $$Revision: 1.14 $$
- * Last changed by $$Author: binary255 $$ on $$Date: 2009/12/28 16:08:58 $$
+ * @version $$Revision: 1.15 $$
+ * Last changed by $$Author: binary255 $$ on $$Date: 2010/07/06 08:44:16 $$
  */
 public class PartMet extends MetFile {
 	
@@ -127,198 +127,203 @@ public class PartMet extends MetFile {
 	}
 	
 	public String getAbsolutePath() {
-		
 		return part_file.getAbsolutePath();
 		
 	}
 	
-	public synchronized void loadFile() throws PartMetException{
-		try {
-			
-			fileChannel.position(0);
-			ByteBuffer data;
-			
-			//Load part file version
-			data = Misc.getByteBuffer(1);
-			fileChannel.read(data);
-			this.partFileFormat = data.get(0);
-			if (this.partFileFormat != PARTFILE_VERSION)
-				throw new PartMetException("Unsupported part file");
-			
-			//Load part file modification date
-			data = Misc.getByteBuffer(4);
-			fileChannel.read(data);
-			this.modDate = data.getInt(0);
-			
-			//Load file hash
-			data = Misc.getByteBuffer(16);
-			fileChannel.read(data);
-			fileHash = new FileHash(data.array());
-			fileHashSet = new PartHashSet(fileHash);
-			
-			//Read part count
-			data = Misc.getByteBuffer(2);
-			fileChannel.read(data);
-			short partCount = data.getShort(0);
-			
-			data = Misc.getByteBuffer(16);
-			for(int i = 0 ; i <partCount; i++){
-				data.clear();
-				fileChannel.read(data);
-				fileHashSet.add(data.array());
-			}
-			
-			//Read tag count
-			data = Misc.getByteBuffer(4);
-			fileChannel.read(data);
-			int tagCount = data.getInt(0);
-			//Load Tags
-			this.tagList = new TagList();
-			
-			for(int i = 0 ; i < tagCount; i++) {
+	private Object lock = new Object();
+	
+	public void loadFile() throws PartMetException{
+		synchronized (lock) {
+			try {
 				
-				Tag tag = TagScanner.scanTag(fileChannel);
-				if (tag != null)
-					tagList.addTag(tag);
-				else {
-					System.out.println("Null tag!");
-					throw new PartMetException("Corrupted tag list in file : " + file.getName() );
+				fileChannel.position(0);
+				ByteBuffer data;
+				
+				//Load part file version
+				data = Misc.getByteBuffer(1);
+				fileChannel.read(data);
+				this.partFileFormat = data.get(0);
+				if (this.partFileFormat != PARTFILE_VERSION)
+					throw new PartMetException("Unsupported part file");
+				
+				//Load part file modification date
+				data = Misc.getByteBuffer(4);
+				fileChannel.read(data);
+				this.modDate = data.getInt(0);
+				
+				//Load file hash
+				data = Misc.getByteBuffer(16);
+				fileChannel.read(data);
+				fileHash = new FileHash(data.array());
+				fileHashSet = new PartHashSet(fileHash);
+				
+				//Read part count
+				data = Misc.getByteBuffer(2);
+				fileChannel.read(data);
+				short partCount = data.getShort(0);
+				
+				data = Misc.getByteBuffer(16);
+				for(int i = 0 ; i <partCount; i++){
+					data.clear();
+					fileChannel.read(data);
+					fileHashSet.add(data.array());
 				}
-			}		
-			gapList = new GapList();
-			byte tag_id = E2DKConstants.GAP_OFFSET;
-			Tag start_tag, end_tag;
-			while (true) {
-				start_tag = tagList.getTag(new byte[]{FT_GAPSTART[0],tag_id});
-				if (start_tag == null) break;
-				end_tag = tagList.getTag(new byte[]{FT_GAPEND[0],tag_id});
-				if (end_tag == null)
-					throw new PartMetException("Can't find end of gap in file partial file ");
-				tagList.removeTag(start_tag.getTagName());
-				tagList.removeTag(end_tag.getTagName());
-				try {
-					long begin = Convert.intToLong((Integer)start_tag.getValue());
-					long end = Convert.intToLong((Integer)end_tag.getValue());
-					gapList.addGap(begin,end );
-				} catch (Throwable e) {
-					throw new PartMetException("Failed to extract gap positions form file : " + file.getName());
+				
+				//Read tag count
+				data = Misc.getByteBuffer(4);
+				fileChannel.read(data);
+				int tagCount = data.getInt(0);
+				//Load Tags
+				this.tagList = new TagList();
+				
+				for(int i = 0 ; i < tagCount; i++) {
+					
+					Tag tag = TagScanner.scanTag(fileChannel);
+					if (tag != null)
+						tagList.addTag(tag);
+					else {
+						System.out.println("Null tag!");
+						throw new PartMetException("Corrupted tag list in file : " + file.getName() );
+					}
+				}		
+				gapList = new GapList();
+				byte tag_id = E2DKConstants.GAP_OFFSET;
+				Tag start_tag, end_tag;
+				while (true) {
+					start_tag = tagList.getTag(new byte[]{FT_GAPSTART[0],tag_id});
+					if (start_tag == null) break;
+					end_tag = tagList.getTag(new byte[]{FT_GAPEND[0],tag_id});
+					if (end_tag == null)
+						throw new PartMetException("Can't find end of gap in file partial file ");
+					tagList.removeTag(start_tag.getTagName());
+					tagList.removeTag(end_tag.getTagName());
+					try {
+						long begin = Convert.intToLong((Integer)start_tag.getValue());
+						long end = Convert.intToLong((Integer)end_tag.getValue());
+						gapList.addGap(begin,end );
+					} catch (Throwable e) {
+						throw new PartMetException("Failed to extract gap positions form file : " + file.getName());
+					}
+					tag_id++;
 				}
-				tag_id++;
+			} catch (FileNotFoundException e) {
+				throw new PartMetException("Failed to load PartFile ");
+			} catch (IOException e) {
+				throw new PartMetException("Failed to read data from PartFile ");
+			} catch(Throwable t) {
+				throw new PartMetException(Misc.getStackTrace(t));
 			}
-		} catch (FileNotFoundException e) {
-			throw new PartMetException("Failed to load PartFile ");
-		} catch (IOException e) {
-			throw new PartMetException("Failed to read data from PartFile ");
-		} catch(Throwable t) {
-			throw new PartMetException(Misc.getStackTrace(t));
 		}
 	}
 
-	public synchronized void writeFile() throws PartMetException {
-		try {
-			fileChannel.position(0);
-			ByteBuffer data;
-			
-			data = Misc.getByteBuffer(1);
-			data.put(PARTFILE_VERSION);
-			data.position(0);
-			fileChannel.write(data);
-			
-			data = Misc.getByteBuffer(4);
-			this.modDate = Convert.longToInt(System.currentTimeMillis());
-			data.putInt(0, this.modDate);
-			data.position(0);
-			fileChannel.write(data);
-			
-			if (fileHash != null) {
-			data = Misc.getByteBuffer(16);
-			data.put(fileHash.getHash());
-			data.position(0);
-			fileChannel.write(data);
-			} else {
+	public void writeFile() throws PartMetException {
+		synchronized (lock) {
+			try {
+				fileChannel.position(0);
+				ByteBuffer data;
+				
+				data = Misc.getByteBuffer(1);
+				data.put(PARTFILE_VERSION);
+				data.position(0);
+				fileChannel.write(data);
+				
+				data = Misc.getByteBuffer(4);
+				this.modDate = Convert.longToInt(System.currentTimeMillis());
+				data.putInt(0, this.modDate);
+				data.position(0);
+				fileChannel.write(data);
+				
+				if (fileHash != null) {
 				data = Misc.getByteBuffer(16);
+				data.put(fileHash.getHash());
 				data.position(0);
 				fileChannel.write(data);
-			}
-			
-			if (fileHashSet != null) {
-				data = Misc.getByteBuffer(2);
-				data.putShort(Convert.intToShort(fileHashSet.size()));
-				data.position(0);
-				fileChannel.write(data);
+				} else {
+					data = Misc.getByteBuffer(16);
+					data.position(0);
+					fileChannel.write(data);
+				}
 				
-				data = Misc.getByteBuffer(16*fileHashSet.size());
-				
-				for(int i = 0; i <fileHashSet.size();i++)
-					data.put(fileHashSet.get(i));
-				
-				data.position(0);
-				fileChannel.write(data);
-			} else {
-				try {
-					long file_size = (Integer)tagList.getTag(FT_FILESIZE).getValue();
-					int part_count = (int)(file_size / PARTSIZE);
-					if ((file_size % PARTSIZE) != 0)
-						part_count++;
+				if (fileHashSet != null) {
 					data = Misc.getByteBuffer(2);
-					data.putShort(Convert.intToShort(part_count));
+					data.putShort(Convert.intToShort(fileHashSet.size()));
 					data.position(0);
 					fileChannel.write(data);
 					
-					data = Misc.getByteBuffer(16);
-					for(int i = 0;i<part_count;i++) {
+					data = Misc.getByteBuffer(16*fileHashSet.size());
+					
+					for(int i = 0; i <fileHashSet.size();i++)
+						data.put(fileHashSet.get(i));
+					
+					data.position(0);
+					fileChannel.write(data);
+				} else {
+					try {
+						long file_size = (Integer)tagList.getTag(FT_FILESIZE).getValue();
+						int part_count = (int)(file_size / PARTSIZE);
+						if ((file_size % PARTSIZE) != 0)
+							part_count++;
+						data = Misc.getByteBuffer(2);
+						data.putShort(Convert.intToShort(part_count));
 						data.position(0);
 						fileChannel.write(data);
+						
+						data = Misc.getByteBuffer(16);
+						for(int i = 0;i<part_count;i++) {
+							data.position(0);
+							fileChannel.write(data);
+						}
+					} catch (Throwable e) {
+						e.printStackTrace();
 					}
-				} catch (Throwable e) {
-					e.printStackTrace();
+					
 				}
 				
-			}
-			
-			/**Count Gaps */
-			int gapCount=gapList.size();
-			data = Misc.getByteBuffer(4);
-			data.putInt(tagList.size()+gapCount*2);
-			data.position(0);
-			fileChannel.write(data);
-			
-			data = Misc.getByteBuffer(tagList.getByteSize());
-						
-			for(Tag tag : tagList) {
-				data.put(tag.getAsByteBuffer());
-			}
-			
-			data.position(0);
-			fileChannel.write(data);
-			
-			/**Write Gap List*/
-			byte counter = E2DKConstants.GAP_OFFSET;
-			byte metaTagBegin[] = FT_GAPSTART.clone();
-			byte metaTagEnd[] = FT_GAPEND.clone();
-			JMIterable<Gap> gap_list = gapList.getGaps();
-			for(Gap gap : gap_list){
-								
-				metaTagBegin[1] = counter;
-				Tag tagBegin = new IntTag(metaTagBegin,Convert.longToInt(gap.getStart()));
-								
-				metaTagEnd[1]=counter;
-				Tag tagEnd = new IntTag(metaTagEnd,Convert.longToInt(gap.getEnd()));
-
+				/**Count Gaps */
+				int gapCount=gapList.size();
+				data = Misc.getByteBuffer(4);
+				data.putInt(tagList.size()+gapCount*2);
+				data.position(0);
+				fileChannel.write(data);
 				
-				data = Misc.getByteBuffer(tagBegin.getSize()+tagEnd.getSize());
-				data.put(tagBegin.getAsByteBuffer());
-				data.put(tagEnd.getAsByteBuffer());
+				data = Misc.getByteBuffer(tagList.getByteSize());
+							
+				for(Tag tag : tagList) {
+					data.put(tag.getAsByteBuffer());
+				}
 				
 				data.position(0);
 				fileChannel.write(data);
 				
-				counter++;
+				/**Write Gap List*/
+				byte counter = E2DKConstants.GAP_OFFSET;
+				byte metaTagBegin[] = FT_GAPSTART.clone();
+				byte metaTagEnd[] = FT_GAPEND.clone();
+				JMIterable<Gap> gap_list = gapList.getGaps();
+				for(Gap gap : gap_list){
+									
+					metaTagBegin[1] = counter;
+					Tag tagBegin = new IntTag(metaTagBegin,Convert.longToInt(gap.getStart()));
+									
+					metaTagEnd[1]=counter;
+					Tag tagEnd = new IntTag(metaTagEnd,Convert.longToInt(gap.getEnd()));
+	
+					
+					data = Misc.getByteBuffer(tagBegin.getSize()+tagEnd.getSize());
+					data.put(tagBegin.getAsByteBuffer());
+					data.put(tagEnd.getAsByteBuffer());
+					
+					data.position(0);
+					fileChannel.write(data);
+					
+					counter++;
+				}
+			} catch (FileNotFoundException e) {
+				throw new PartMetException("Failed to open for writing part file : " +part_file.getName());
+			} catch (IOException e) {
+				throw new PartMetException("Failed to write dta in part file : "+part_file.getName());	
 			}
-		} catch (FileNotFoundException e) {
-			throw new PartMetException("Failed to open for writing part file : " +part_file.getName());
-		} catch (IOException e) {
-			throw new PartMetException("Failed to write dta in part file : "+part_file.getName());	
 		}
 		
 	}
